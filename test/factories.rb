cbaee5ec4acc383b38a0e6a7554607e629b8021c
Sean Doyle <sean.p.doyle24@gmail.com>
2019-05-25 13:51:33 -0400

    Add System Test for viewing a Seat's details
    This commit introduces `factory_bot` and configures MiniTest to use it
    instead of loading our application's fixture files.
    
    The first test drives the browser by visiting the Benedum Center's
    Orchestra floor, then clicking on seat `AA-101`.
    
    The `click_on_seat` help is necessary due to Capybara's inability to
    treat `<a>` elements nested within `<svg>` elements as if they were
    normal `<a>` elements. Capybara's selector strategies include a matcher
    that finds `<a>` elements based on their `title` attribute text, or
    their `aria-label` attribute text. However, when matching elements exist
    within an `<svg>`, they are not found.
    
    To counteract this limitation, the `click_on_seat` uses an `aria-label`
    attribute CSS selector to determine which element to click.

---
+++ test/factories.rb
@@ -0,0 +1,30 @@
+FactoryBot.define do
+  factory :benedum_center, class: "Venue" do
+    name { "Benedum Center" }
+    slug { "benedum_center" }
+  end
+
+  factory :orchestra, class: "Floor" do
+    association :venue, factory: [:benedum_center]
+
+    name { "Orchestra" }
+    slug { "orchestra" }
+  end
+
+  factory :section do
+    association :floor, factory: [:orchestra]
+
+    name { "Section #{id}" }
+    slug { "section-#{id}" }
+    price { 10_00 }
+  end
+
+  factory :seat do
+    association :section
+
+    sequence(:number)
+    row { "AA" }
+    x { 0 }
+    y { 0 }
+  end
+end
