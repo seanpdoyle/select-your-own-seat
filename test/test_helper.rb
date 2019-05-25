615091b292a2a506674f32bfdeb8da1725a673fd
Sean Doyle <sean.p.doyle24@gmail.com>
2019-05-25 13:51:33 -0400

    Add System Test for viewing a Seat's details
    This commit introduces `factory_bot` and configures MiniTest to use it
    instead of loading our application's fixture files.
    
    The first test drives the browser by visiting the Benedum Center's
    Orchestra floor, then clicking on seat `AA-101`.
    
    The `click_on_seat` help is necessary due to Capybara's inability to
    treat `<a>` elements nested within `<svg>` elements as if they were
    normal `<a>` elements. Capybara's selector strategies include a matcher
    that finds `<a>` elements based on their `title` attribute text, or
    their `aria-label` attribute text. However, when matching elements exist
    within an `<svg>`, they are not found.
    
    To counteract this limitation, the `click_on_seat` uses an `aria-label`
    attribute CSS selector to determine which element to click.
    
    Running the tests
    ---
    
    To simplify executing the entire test suite, this commit declares the
    `default` Rake task to execute _both_ [`rails test`][test] and [`rails
    test:system`][system-test].
    
    This diff intentionally commits a failing test suite to the project's
    revisions history. It represents the "Red" phase of a ["Red, Green,
    Refactor" cycle][tdd-cycle], and will be resolved in future commits.
    
    [default-task]: https://github.com/ruby/rake/tree/v13.0.1#label-Usage
    [test]: https://guides.rubyonrails.org/v6.0/command_line.html#rails-test
    [system-test]: https://guides.rubyonrails.org/v6.0/testing.html#implementing-a-system-test
    [tdd-cycle]: https://en.wikipedia.org/wiki/Test-driven_development#Development_style

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,8 +6,9 @@ class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  include FactoryBot::Syntax::Methods
+
+  FactoryBot.find_definitions
 
   # Add more helper methods to be used by all tests here...
 end
