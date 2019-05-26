4d579e82354be2b9262ab15c72db6cff77ab5404
Sean Doyle <sean.p.doyle24@gmail.com>
2019-05-26 14:46:49 -0400

    Unselect a Seat from the `<dialog>`
    When viewing the details for a `Seat` that is already included in the
    visitor's `Cart`, present them with a button to remove it.
    
    In order to support the removal of a Seat without looking up the
    intermediary `SeatSelection` record, modify the `resources
    :seat_selections` route declaration's `destroy` action to be a singular
    resource instead.
    
    This means that instead of handling `DELETE
    /seats/:seat_id/selections/:id` requests, the `selections#destroy`
    controller action will handle `DELETE /seats/:seat_id/selections`
    requests, determining the correct `SeatSelection` record based on the
    context from the `Current.cart` and the `params[:seat_id]`.
    
    [singular-resource]: https://guides.rubyonrails.org/v5.2/routing.html#singular-resources

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,8 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  def click_on_seat(row_number)
+    find(%{[aria-label*="#{row_number}"]}).click
+  end
 end
