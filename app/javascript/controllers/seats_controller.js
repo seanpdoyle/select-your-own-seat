20d3bec86c3a188d24542f4b7f4215a3e601335d
Sean Doyle <sean.p.doyle24@gmail.com>
2019-05-26 22:12:01 -0400

    Add panning and zooming to SVG map element
    This commit adds a dependency on the [`svg-pan-zoom`
    package][svg-pan-zoom].
    
    To manage the configuration and execution of that plugin, this commit
    introduces `controllers/seats_controller` JavaScript module. [That
    module will be instantiated with elements that declare
    `data-controller="seats"`][controllers].
    
    Once an element with `data-controller="seats"` exists on the page, the
    module's [`connect()` life cycle hook][connect] will be invoked, which
    will initiate the `svgPanZoom` plugin configuration, passing a reference
    to `this.element` as the DOM Node to attach the map to.
    
    Additionally, This commit wraps the collection of `<use>` elements in an
    outer [`<g>` element][g-element] (as suggested by
    [aritutta/svg-pan-zoom#146]).
    
    Desktop
    ---
    
    ![panning-zooming-click](https://user-images.githubusercontent.com/2575027/77386342-fa309f80-6d60-11ea-84b5-8b1e49386809.gif)
    
    [controllers]: https://stimulusjs.org/reference/controllers
    [connect]: https://stimulusjs.org/reference/lifecycle-callbacks#connection
    [svg-pan-zoom]: https://github.com/ariutta/svg-pan-zoom/tree/3.6.0
    [g-element]: https://developer.mozilla.org/en-US/docs/Web/SVG/Element/g
    [aritutta/svg-pan-zoom#146]: https://github.com/ariutta/svg-pan-zoom/issues/146

---
+++ app/javascript/controllers/seats_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "stimulus"
+import svgPanZoom from "svg-pan-zoom"
+
+export default class extends Controller {
+  static targets = [
+    "map",
+  ]
+
+  connect() {
+    this.map = svgPanZoom(this.mapTarget, {
+      viewportSelector: "#map-viewport",
+      center: true,
+      fit: true,
+      zoomEnabled: false,
+      zoomScaleSensitivity: 0.75,
+      minZoom: 1.0,
+      maxZoom: 8,
+    })
+  }
+
+  disconnect() {
+    this.map.destroy()
+  }
+}
