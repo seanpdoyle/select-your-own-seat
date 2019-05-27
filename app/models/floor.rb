3016ad27f87c58e96fb122f3ae39de434cdcb573
Sean Doyle <sean.p.doyle24@gmail.com>
2019-05-26 20:25:10 -0400

    Add static HTML elements for Price Filtering
    The application this example repository is imitating priced Seats in
    bulk, grouped by their Sections.
    
    This commit adds static HTML to render a set of [`<input type="radio">`
    elements][mdn-radio] styled as buttons with `<label>` elements that wrap
    `<svg>` elements. These `<input type="radio">` buttons will serve as a
    means of browsing available seats based on the _maximum price_ the user
    is willing to pay for a ticket.
    
    ---
    
    The selection of a [filter `<input type="radio">` element][mdn-radio]
    does not yet affect the page, but will be integral to implementing that
    behavior in subsequent commits.
    
    The values have been chosen as `500` cents ($5.00), `1000` cents
    ($10.00), and `1500` cents ($15.00). They've been selected since they
    correspond the current possible values of `sections.price` rows in the
    database. They're [underscored to resemble their price in
    dollars][number], and are iterated in  pairs consecutively.
    
    The `Floor#price_ranges` method wraps those values into a set of
    consecutive [`Range` instances][ruby-range], ending in an [endless
    `Range`][endless-range] from `15_00` to infinity.
    
    In the `seats#index` template, those `Range` instances are iterated over
    and translated into `<input type="radio">` elements that represent their
    lower bounds.
    
    Desktop
    ---
    
    ![price filtering form](https://user-images.githubusercontent.com/2575027/77386361-03217100-6d61-11ea-9115-9c69a33308f3.png)
    
    [mdn-radio]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
    [each_cons]: https://ruby-doc.org/core-2.6.3/Enumerable.html#method-i-each_cons
    [checked]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/Input/radio#checked
    [number]: https://docs.ruby-lang.org/en/2.6.0/syntax/literals_rdoc.html#label-Numbers
    [ruby-range]: https://ruby-doc.org/core-2.7.1/Range.html
    [endless-range]: https://ruby-doc.org/core-2.7.1/Range.html#class-Range-label-Beginless-2FEndless+Ranges
    [cover]: https://ruby-doc.org/core-2.7.1/Range.html#method-i-cover-3F

--- app/models/floor.rb
+++ app/models/floor.rb
@@ -4,6 +4,14 @@ class Floor < ApplicationRecord
   has_many :sections
   has_many :seats, through: :sections
 
+  def price_ranges
+    [
+      (5_00...10_00),
+      (10_00...15_00),
+      (15_00...)
+    ]
+  end
+
   def to_param
     slug
   end
