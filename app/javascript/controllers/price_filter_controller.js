261ecaaa373da3dcaf63c5fb5eb74fb86bf3fd74
Sean Doyle <sean.p.doyle24@gmail.com>
2019-06-01 11:11:01 -0400

    Persist price filters to URL query parameters
    This commit [modifies the current page's URL][history] through the
    [`window.history.replaceState` API][replaceState].
    
    When a filter is selected, modify the [current URL's
    `searchParams`][searchParams], setting the `?maximum=` query parameter
    when there is a filter, and removing it when the form is cleared.
    
    This progressively enhances the page's experience for those with
    JavaScript enabled. Without JavaScript enabled, clicking the `<form>`
    element's `<input type="submit">` element wrapped in a `<noscript>`
    element will be behave similarly.
    
    Most importantly, when the user refreshes the page, the URL will
    preserve their filtering selections, and the page will render
    highlighting _only_ the seats within the selected filter.
    
    ---
    
    Within the context of the rest of this example application, this is a
    somewhat extreme and superfluous client-side enhancement. We're hewing
    close to re-implementing server-side logic.
    
    What's worth highlighting is that we're thinking in terms of URLs and
    page reloads, it just so happens that we have an opportunity to improve
    the performance and experience in a JavaScript-capable environment.
    
    [history]: https://developer.mozilla.org/en-US/docs/Web/API/History_API#Adding_and_modifying_history_entries
    [replaceState]: https://developer.mozilla.org/en-US/docs/Web/API/History/replaceState
    [searchParams]: https://developer.mozilla.org/en-US/docs/Web/API/URL/searchParams

--- app/javascript/controllers/price_filter_controller.js
+++ app/javascript/controllers/price_filter_controller.js
@@ -3,6 +3,15 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   apply({ target }) {
     const maximum = target.value
+    const currentUrl = new URL(document.location)
+
+    if (maximum) {
+      currentUrl.searchParams.set("maximum", maximum)
+    } else {
+      currentUrl.searchParams.delete("maximum")
+    }
+
+    history.replaceState({}, "", currentUrl)
 
     this.data.set("maximum", maximum)
   }
