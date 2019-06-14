48a6e2462b2bd67a7bc8f7c932a1c2f7cf3168a0
Sean Doyle <sean.p.doyle24@gmail.com>
2019-06-14 16:32:23 -0400

    Introduce Seats Page Frame
    This commit adds a `SeatsController` class to serve requests made to
    routes like:
    
        /venues/benedum_center/floors/orchestra/seats
    
    This commit relies upon Rails' conventional controller action rendering
    logic.
    
    To declare a controller "action", a controller that extends from
    `ActionController::Base` can either:
    
    * define an instance method that corresponds to the action (in this
      case, `index`)
    * declare a view file named after the action in the directory that
      corresponds to the controller name (in this case,
      `notes/index.html.erb`)
    
    If a method that corresponds to the action does not exist, Rails will
    automatically [infer which template to render][template].
    
    The controller has no methods defined, and declares a `seats#index` view
    template in the `app/views/seats/index.html.erb` file. The `seats#index`
    renders static HTML that relies upon the pre-defined CSS styling rules.
    
    This static HTML serves as the Seats page's frame. We'll make the
    contents of the page dynamic in subsequent commits.
    
    [template]: https://guides.rubyonrails.org/v5.2/layouts_and_rendering.html#rendering-by-default-convention-over-configuration-in-action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :venues, only: [] do
+    resources :floors, only: [] do
+      resources :seats, only: [:index]
+    end
+  end
+
+  root to: redirect("/venues/benedum_center/floors/orchestra/seats")
 end
