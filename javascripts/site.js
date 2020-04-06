26a0ba4a8b642aaffd3a3989e9b3a5e14b0be8b0
seanpdoyle <seanpdoyle@users.noreply.github.com>
2020-04-06 17:07:40 +0000

    Create `git-read` GitHub Action
    Utilize the [`seanpdoyle/git-read`][git-read] [GitHub Action] to
    generate web pages from this project's Git history.
    
    The declarations in `.github/workflow/git-read.yml` were inspired by the
    corresponding GitHub Action workflow declared in the [`git-read`
    repository][git-read-workflow].
    
    Specifying the `--git-dir` and `--output-dir` values in terms of the
    GitHub Actions-provided [`$GITHUB_WORKSPACE` environment variable][env]
    is important to ensure that the proper output is pushed to the
    `gh-pages` branch.
    
    Upon each `git push` to this repository, this project's Git history will
    be rendered as HTML and deployed to the [`gh-pages` branch][gh-pages],
    available at https://seanpdoyle.github.io/select-your-own-seat.
    
    [git-read]: https://github.com/seanpdoyle/git-read
    [gh-action]: https://github.com/features/actions
    [git-read-workflow]: https://github.com/seanpdoyle/git-read/blob/master/.github/workflows/git-read.yml
    [env]: https://help.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables#default-environment-variables
    [gh-pages]: https://pages.github.com/ fae3c14b15f5d24a52167957f26eeffaf592e20b

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,22 @@
-// This is where it all goes :)
+import "details-element-polyfill"
+import Turbolinks from "turbolinks"
+
+Turbolinks.start()
+
+const scrollToCurrentCommit = (element) => {
+  for (const current of element.querySelectorAll('[aria-current="page"]')) {
+    current.scrollIntoView({ block: "center", inline: "center" })
+  }
+}
+
+document.addEventListener("turbolinks:load", () => {
+  for (const details of document.querySelectorAll('details[data-controller*="commit-history"]')) {
+    details.addEventListener("toggle", (event) => {
+      if (details.open) {
+        scrollToCurrentCommit(details)
+      }
+    })
+  }
+
+  scrollToCurrentCommit(document)
+})
